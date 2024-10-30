6382f9ffdc50b209cfcf974ac557a218d2792b57
kiptoobarchok <calebkiptoo9090@gmail.com>
2024-10-30 10:12:45 +0000

    lint error

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 
 export default class ClassRoom {
-    constructor(maxStudentsSize){
-        this._maxStudentsSize = maxStudentsSize;
-    }
-    set maxStudentsSize(newMaxStudentSize) {
-        if (typeof newMaxStudentSize === 'number') {
-            this._maxStudentsSize = newMaxStudentSize;
-        }
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 
-    get maxStudentsSize() {
-        return this._maxStudentsSize;
+  set maxStudentsSize(newMaxStudentSize) {
+    if (typeof newMaxStudentSize === 'number') {
+      this._maxStudentsSize = newMaxStudentSize;
     }
+  }
+
+  get maxStudentsSize() {
+    return this._maxStudentsSize;
+  }
 }
