3efa8dc09ddbc79981be866e1f0b7bb89afebcc5
kiptoobarchok <calebkiptoo9090@gmail.com>
2024-11-04 10:44:15 +0000

    data manipulation

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(objectsArray){
+  if (!Array.isArray(objectsArray)) {
+      return [];
+  }
+
+	const ids = objectsArray.map((x) => x.id);
+	return ids;
+}
